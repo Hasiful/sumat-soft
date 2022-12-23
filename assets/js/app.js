4e8aa4fc6e2c2de03cda3cc978e1d88fd404de84
hasiful <hasifulislam32@gmail.com>
2022-12-23 23:31:12 +0600

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+// sticky header
+var header = document.getElementsByClassName("header_nav_sticky")[0];
+var sticky = header.offsetTop;
+
+
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+})
