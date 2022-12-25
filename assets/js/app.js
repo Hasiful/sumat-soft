f1fa1be3f44af120a13fe7d9faaf900709eadd08
hasiful <hasifulislam32@gmail.com>
2022-12-26 00:49:51 +0600

    saturday work

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,16 @@
+// about slider
+    $('.about_slider').slick({
+        dots: true,
+        arrows: true,
+        autoplay: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+    });
+
 // sticky header
+
 var header = document.getElementsByClassName("header_nav_sticky")[0];
 var sticky = header.offsetTop;
 
