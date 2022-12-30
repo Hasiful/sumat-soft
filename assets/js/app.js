9e115d2ebe635b600f8f0a4099cd093caa900b2e
hasiful <hasifulislam32@gmail.com>
2022-12-31 00:16:51 +0600

    responsive header footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,3 +32,33 @@ window.addEventListener('scroll', function () {
         header.classList.remove("sticky");
     }
 })
+
+// toggle nav show
+
+{
+    let toggleBtn = document.getElementById("toggle_btn")
+    let mobileNav = document.getElementById("mobile_nav")
+
+    toggleBtn.addEventListener("click", function(){
+        mobileNav.classList.toggle("showmobileNav")
+    })
+}
+
+// mobile nav
+
+{
+    let navHeading = document.getElementsByClassName("mobilenav_item_wrapper")
+    ;[...navHeading].forEach((currentItem,index) => {
+        let mobileSubNav = document.getElementsByClassName("mobilesub_navber")[index]
+
+        currentItem.addEventListener("click", function () {
+
+            let showMobileClass = document.querySelector(".showMobileSub")
+
+            if (!mobileSubNav.classList.contains("showMobileSub") && showMobileClass) {
+                showMobileClass.classList.remove("showMobileSub")
+            }
+            mobileSubNav.classList.toggle("showMobileSub")
+        })
+    });
+}
